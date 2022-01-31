aa7dc7cf673ee9dc404b7603fb6b42606f33bc3e
Øystein Schønning-Johansen <oysteijo@gmail.com>
2022-01-31 21:55:18 +0100

    Adding option for shared or static builds

--- example/example.c
+++ example/example.c
@@ -1,32 +1,33 @@
-#include "npy_array.h"
-#include "npy_array_list.h"
-#include <stdio.h>
-int main(int argc, char *argv[])
-{
-    if( argc != 2 ){
-        printf("Usage: %s <filename>\n\nProvided filename is supposed to contain a numpy matrix\n", argv[0]);
-        return -1;
-    }
+#include <npy_array.h>
+#include <npy_array_list.h>
 
-    npy_array_list_t *list = npy_array_list_load( argv[1] );
-    size_t len = npy_array_list_length( list );
-    printf("Length: %d\n", (int) len );
-    if( len == 0 ){
-        /* This is possibly a matrix stored with 'save' instead of 'savez' */
-        npy_array_t *arr = npy_array_load( argv[1] );
-        if( !arr ){
-            printf("nope! No array there!\n");
+#include <stdio.h>
+    int main(int argc, char *argv[])
+    {
+        if( argc != 2 ){
+            printf("Usage: %s <filename>\n\nProvided filename is supposed to contain a numpy matrix\n", argv[0]);
             return -1;
         }
 
-        npy_array_dump( arr );
-        npy_array_free( arr );
-        return 0;
-    }
+        npy_array_list_t *list = npy_array_list_load( argv[1] );
+        size_t len = npy_array_list_length( list );
+        printf("Length: %d\n", (int) len );
+        if( len == 0 ){
+            /* This is possibly a matrix stored with 'save' instead of 'savez' */
+            npy_array_t *arr = npy_array_load( argv[1] );
+            if( !arr ){
+                printf("nope! No array there!\n");
+                return -1;
+            }
 
-    printf("number of objects in file: %lu\n", len);
-    int filename_counter = 0;
-    for ( npy_array_list_t *iter = list; iter; iter = iter->next ){
+            npy_array_dump( arr );
+            npy_array_free( arr );
+            return 0;
+        }
+
+        printf("number of objects in file: %lu\n", len);
+        int filename_counter = 0;
+        for ( npy_array_list_t *iter = list; iter; iter = iter->next ){
         npy_array_dump( iter->array );
         char fn[30];
         sprintf( fn, "array_%d.npy", filename_counter++ );
