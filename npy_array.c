80a0b2cd501f32eea1643d3a040450afd3613b84
Lennart Klebl <15572276+alcubierre-drive@users.noreply.github.com>
2024-09-20 17:16:42 +0200

    add interface to build npy_array_list objects from C (#30)
    * add interface to build npy_array_list objects from C
    
    * rename to deepcopy
    
    ---------
    
    Co-authored-by: Lennart Klebl (alcubierre-drive)

--- npy_array.c
+++ npy_array.c
@@ -45,7 +45,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ctype.h>
 #include <assert.h>
 
-#define NPY_ARRAY_MAGIC_STRING {0x93,'N','U','M','P','Y'}
+#define NPY_ARRAY_MAGIC_STRING {-109,'N','U','M','P','Y'}
 #define NPY_ARRAY_MAJOR_VERSION_IDX 6
 #define NPY_ARRAY_MINOR_VERSION_IDX 7
 
@@ -396,4 +396,25 @@ void npy_array_free( npy_array_t *m )
         free( m->data );
 
     free( m );
+}
+
+#define MIN( x, y ) ((x) < (y) ? (x) : (y))
+npy_array_t* npy_array_deepcopy( const npy_array_t* m ) {
+    npy_array_t* ary = calloc( 1, sizeof(*ary) );
+    if (!ary) {
+        fprintf(stderr, "Cannot allocate data structure!\n");
+        return NULL;
+    }
+    ary->ndim = MIN( m->ndim, NPY_ARRAY_MAX_DIMENSIONS );
+    memcpy( ary->shape, m->shape, sizeof(ary->shape) );
+    ary->typechar = m->typechar;
+    ary->elem_size = m->elem_size;
+    ary->data = malloc( npy_array_calculate_datasize(ary) );
+    if (!ary->data) {
+        fprintf(stderr, "Cannot allocate memory!\n");
+        free(ary);
+        return NULL;
+    }
+    memcpy( ary->data, m->data, npy_array_calculate_datasize(ary) );
+    return ary;
 }
